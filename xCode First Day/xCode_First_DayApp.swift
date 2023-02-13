bb4210bfd0f46d90658e62c6c1ba2b23380dccc2
ArtemLyakhovetskyy <115452070+ArtemLyakhovetskyy@users.noreply.github.com>
2023-02-13 10:49:15 +0100

    Initial Commit

---
+++ xCode First Day/xCode_First_DayApp.swift
@@ -0,0 +1,17 @@
+//
+//  xCode_First_DayApp.swift
+//  xCode First Day
+//
+//  Created by Artem Lyakhovetskyy on 13.02.23.
+//
+
+import SwiftUI
+
+@main
+struct xCode_First_DayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
